fe1721224168d3c341fe6a099ef105e00b88905c
jedikarix <karolchecinski@wp.pl>
2018-05-09 02:19:53 +0200

    Make SocketWrapper class virtual

--- server/src/SocketWrapper.cpp
+++ server/src/SocketWrapper.cpp
@@ -5,28 +5,62 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <stdexcept>
+#include <unistd.h>
 #include <errno.h>
 
 
 SocketWrapper::SocketWrapper(unsigned port)
     :port_(port)
+    ,socket_fd_(UNINIT_FD)
 {
     init();
+}
+
+SocketWrapper::SocketWrapper(SocketWrapper &&move) noexcept
+    :port_(0)
+{
+    move.swap(*this);
+}
+
+SocketWrapper &SocketWrapper::operator=(SocketWrapper &&move) noexcept
+{
+    move.swap(*this);
+    return *this;
+}
+
+SocketWrapper::~SocketWrapper()
+{
+    close();
+}
+
+void SocketWrapper::close()
+{
+    if (socket_fd_ == UNINIT_FD)
+        return;
+    ::close(socket_fd_);
+    //TODO close errors
+}
+
+void SocketWrapper::swap(SocketWrapper &other) noexcept
+{
+    std::swap(port_, other.port_);
 }
 
 void SocketWrapper::init()
 {
     socket_fd_ = socket(AF_INET, SOCK_STREAM, 0);
-    int yes = 1;
-    if (setsockopt(socket_fd_,
-                   SOL_SOCKET,
-                   SO_REUSEADDR,
-                   &yes,
-                   sizeof(int)) == -1) {
-        throw std::runtime_error("setsockopt error: " + errno);
-    }
-
-    serv_addr_.sin_family = AF_INET;
-    serv_addr_.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr_.sin_port = htons(port_);
 }
+
+//    if (setsockopt(socket_fd_,
+//                   SOL_SOCKET,
+//                   SO_REUSEADDR,
+//                   &yes,
+//                   sizeof(int)) == -1) {
+//        throw std::runtime_error("setsockopt error: " + errno);
+//    }
+
+//    serv_addr_.sin_family = AF_INET;
+//    serv_addr_.sin_addr.s_addr = htonl(INADDR_ANY);
+//    serv_addr_.sin_port = htons(port_);
+
+
--- server/src/SocketWrapper.hpp
+++ server/src/SocketWrapper.hpp
@@ -3,22 +3,28 @@
 
 #include <sys/socket.h>
 #include <netdb.h>
-const char* PORT = "4950";
 
 class SocketWrapper {
 
+    static constexpr int UNINIT_FD = -1;
 public:
     SocketWrapper(unsigned port);
-    ~SocketWrapper() = default;
 
+    SocketWrapper(SocketWrapper&& move) noexcept;
+    SocketWrapper& operator=(SocketWrapper&& move) noexcept;
+    void swap(SocketWrapper& other) noexcept;
+    SocketWrapper(SocketWrapper const&) = delete;
+    SocketWrapper& operator=(SocketWrapper const&) = delete;
+
+    virtual ~SocketWrapper();
+
+    void close();
 
 private:
     void init();
 
     int socket_fd_;
     unsigned port_;
-    struct sockaddr_in serv_addr_;
-    struct sockaddr_in client_addr_;
 };
 
 
