c562126b3c59279af9739c3b1584246a6b1e0fe3
Kasia4 <katarzyna.rybacka4@gmail.com>
2018-05-18 00:54:27 +0200

    WIP BlockingQueue

---
+++ server/src/BlockingQueue.hpp
@@ -0,0 +1,29 @@
+#ifndef BLOCKINGQUEUE_HPP
+#define BLOCKINGQUEUE_HPP
+
+#include <mutex>
+#include <condition_variable>
+#include <queque>
+
+#include "cppQ.pb.h"
+
+using Packet = packet::PACKET;
+class BlockingQueue
+{
+    static constexpr unsigned MAX_CAPACITY = 50;
+public:
+    BlockingQueue() : capacity_(MAX_CAPACITY) {};
+    ~BlockingQueue() = default;
+
+    Packet pop();
+    void push(const Packet& packet);
+
+private:
+    std::mutex guard_;
+    std::condition_variable full_;
+    std::condition_variable full_;
+    std::queue<Packet> packet_queue_;
+    unsigned capacity_;
+};
+
+#endif // BLOCKINGQUEUE_HPP
