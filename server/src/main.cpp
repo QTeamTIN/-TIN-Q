7901450da40d32d3ab242dffc65a8edfb8659ec0
jedikarix <karolchecinski@wp.pl>
2018-05-29 12:04:03 +0200

    Get server port from argv

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,8 +1,16 @@
 #include "Server.hpp"
 
-int main()
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char * argv[])
 {
-    Server server(8888);
+    if(argc < 2) {
+        std::cout<<"Port unspecified"<<std::endl;
+        return 1;
+    }
+    int port = std::atoi(argv[1]);
+    Server server(port);
 
     server.run();
 }
