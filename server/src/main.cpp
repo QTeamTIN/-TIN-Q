0f8d79a8cb4202a950098bf9b36b99ba3eb852db
jedikarix <karolchecinski@wp.pl>
2018-05-29 12:08:02 +0200

    Port value validation

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -6,10 +6,15 @@
 int main(int argc, char * argv[])
 {
     if(argc < 2) {
-        std::cout<<"Port unspecified"<<std::endl;
+        std::cout<<"Error: Port unspecified"<<std::endl;
         return 1;
     }
     int port = std::atoi(argv[1]);
+    if (port < 1024) {
+        std::cout<<"Error: Invalid port"<<std::endl;
+        return 1;
+    }
+
     Server server(port);
 
     server.run();
