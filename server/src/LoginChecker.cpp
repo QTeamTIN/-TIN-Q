478530577f9d92b07fb407e1653aeecea3f5a74e
Konrad Linux <konradqcharczyk@gmail.com>
2018-06-05 20:37:43 +0200

    return type of login() changed

--- server/src/LoginChecker.cpp
+++ server/src/LoginChecker.cpp
@@ -7,15 +7,15 @@ LoginChecker::LoginChecker(const Q_DAO &db_handler)
 
 }
 
-std::shared_ptr<UserTuple> LoginChecker::login(const std::string &username, const std::string& hash)
+std::shared_ptr<User> LoginChecker::login(const std::string &username, const std::string& hash)
 {
     try {
-        auto user = std::make_shared<UserTuple>(db_handler_.loadUser(username));
-        if (hash == user->getPassword())
-            return user;
+        auto userTuple = std::make_shared<UserTuple>(db_handler_.loadUser(username));
+        if (hash == userTuple->getPassword())
+            return std::make_shared<User>(userTuple->getUser());
 
     } catch (const std::invalid_argument& e) {
         std::cout<<e.what()<<std::endl;
     }
-    return std::shared_ptr<UserTuple>();
+    return std::shared_ptr<User>();
 }
